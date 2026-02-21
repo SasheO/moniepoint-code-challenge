45c2aa2a15eba356ad5382f91c2372ca9ab7334a
M.S. Ojuba <76496417+SasheO@users.noreply.github.com>
2026-02-21 14:24:30 +0100

    moved sql view code to folder with txt files and renamed api_endpoints to run_server

--- create_views_for_database.py
+++ create_views_for_database.py
@@ -1,67 +1,26 @@
 from connect_to_database import *
 
-# create views for all needed queries
-top_merchant_view = """
-drop view if exists top_merchant;
-CREATE VIEW top_merchant AS
-select merchant_id, sum(amount) AS total_volume from merchant_activity_records
-where status  = 'SUCCESS'
-group by merchant_id
-order by total_volume desc
-limit 1;
-"""
+import os
 
-product_adoption_view = """
-drop view if exists product_adoption;
-CREATE VIEW product_adoption AS
-SELECT product, COUNT(DISTINCT merchant_id) AS merchant_product_count
-FROM merchant_activity_records
-group by product
-order by merchant_product_count desc;
-"""
+# this folder contains .txt files with sql queries that will be run
+input_folder = "views/"
 
-kyc_funnel_view = """
-drop view if exists kyc_funnel;
-CREATE VIEW kyc_funnel AS
-SELECT event_type, COUNT(DISTINCT merchant_id) AS successful_kyc_event_count
-FROM merchant_activity_records
-where product = 'KYC' and status = 'SUCCESS'
-group by event_type;
-"""
+# get all files in input folder path
+files = [
+    input_folder+f for f in os.listdir(input_folder)
+    if os.path.isfile(os.path.join(input_folder, f))
+]
 
-monthly_active_merchants_view = """
-drop view if exists monthly_active_merchants;
-CREATE VIEW monthly_active_merchants AS
-SELECT TO_CHAR(DATE_TRUNC('month', event_timestamp), 'YYYY-MM') AS month, COUNT(DISTINCT merchant_id) AS monthly_active_merchant_count
-FROM merchant_activity_records
--- WHERE status = 'SUCCESS'
-WHERE event_timestamp IS NOT NULL
-GROUP BY month
-HAVING SUM(CASE WHEN status = 'SUCCESS' THEN 1 ELSE 0 END) > 0
-ORDER BY month;
-"""
-
-failure_rates_view = """
-drop view if exists failure_rates;
-CREATE VIEW failure_rates AS
-SELECT product, 
-    ROUND(
-        100.0 * SUM(CASE WHEN status = 'FAILED' THEN 1 ELSE 0 END) / COUNT(*),
-        1
-    ) AS failure_rate
-FROM merchant_activity_records
-where status = 'SUCCESS' or status = 'FAILED'
-group by product
-order by failure_rate desc;
-"""
 
 if __name__ == "__main__":
-    # TODO: Execute the SQL queries for each
-    # below is how to execute for one
+
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute(top_merchant_view)
-    conn.commit()  # commit changes
+    for filename in files:
+        with open(filename, "r") as file:
+            sql_view_query = file.read()      
+        cursor.execute(sql_view_query)
+        conn.commit()
 
     # close connection to database
     cursor.close()
