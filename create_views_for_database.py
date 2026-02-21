b98e1c7561ab928fb5bc59de7757cb3805130739
M.S. Ojuba <76496417+SasheO@users.noreply.github.com>
2026-02-21 13:07:52 +0100

    created more views

--- create_views_for_database.py
+++ create_views_for_database.py
@@ -20,7 +20,7 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 # create views for all needed queries
-top_merchant_query = """
+top_merchant_view = """
 CREATE VIEW top_merchant AS
 select merchant_id, sum(amount) AS total_volume from merchant_activity_records
 where status  = 'SUCCESS'
@@ -29,8 +29,25 @@ order by total_volume desc
 limit 1;
 """
 
-# Execute the SQL
-cursor.execute(top_merchant_query)
+product_adoption_view = """
+CREATE VIEW product_adoption AS
+SELECT product, COUNT(DISTINCT merchant_id) AS merchant_product_count
+FROM merchant_activity_records
+group by product
+order by merchant_product_count desc;
+"""
+
+kyc_funnel_view = """
+CREATE VIEW kyc_funnel AS
+SELECT event_type, COUNT(DISTINCT merchant_id) AS successful_kyc_event_count
+FROM merchant_activity_records
+where product = 'KYC' and status = 'SUCCESS'
+group by event_type;
+"""
+
+# TODO: Execute the SQL queries for each
+# below is how to execute for one
+cursor.execute(top_merchant_view)
 conn.commit()  # commit changes
 
 # close connection to database
